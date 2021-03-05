a32d40afc5c3026b2694d70b27e4ac71c17db925
Erick <erickfrancodelgado@hotmail.com>
2021-03-05 20:38:14 +0100

    création de la class SecurityConfig

---
+++ src/main/java/fr/erickfranco/cv_api/configurations/security/SecurityConfig.java
@@ -0,0 +1,10 @@
+package fr.erickfranco.cv_api.configurations.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
