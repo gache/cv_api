5a6b6bf22ada2bac405952c3389bc77a814e37de
Joker <JOKER.m2i.wetic@gmail.com>
2021-03-05 13:25:48 +0100

    mise à jour de la class UserRepository

--- src/main/java/fr/erickfranco/cv_api/repositories/UserRepository.java
+++ src/main/java/fr/erickfranco/cv_api/repositories/UserRepository.java
@@ -3,9 +3,14 @@ package fr.erickfranco.cv_api.repositories;
 import fr.erickfranco.cv_api.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * @author Erick Franco
+ */
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select u from User u " + "where u.username = ?1")
     Optional<User> findUserWithName(String username); // permet de recuperer un User par son nom utilisateur
