34bb6766e07b56f9bcfb2b738aac79b0563dacfc
Joker <JOKER.m2i.wetic@gmail.com>
2021-03-05 13:45:56 +0100

    Création class LangageServiceInter

---
+++ src/main/java/fr/erickfranco/cv_api/services/serviceinter/LangageServiceInter.java
@@ -0,0 +1,20 @@
+package fr.erickfranco.cv_api.services.serviceinter;
+
+import fr.erickfranco.cv_api.models.Langage;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Erick Franco
+ */
+public interface LangageServiceInter {
+
+    List<Langage> findAllLangage();
+
+    Optional<Langage> findLangageById(Integer id);
+
+    Langage createLangage(Langage langage);
+
+    void deleteLangageById(Integer id);
+}
