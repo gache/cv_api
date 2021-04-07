436be75ac0b968ee94b7b2355511214ae12deda9
gache <erickfrancodelgado@hotmail.com>
2021-04-07 13:34:34 +0200

    nouvelle configuration de gitignore

--- src/main/java/fr/erickfranco/cv_api/CvApiApplication.java
+++ src/main/java/fr/erickfranco/cv_api/CvApiApplication.java
@@ -2,8 +2,12 @@ package fr.erickfranco.cv_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({ @PropertySource(value = "classpath:application.properties"),
+        @PropertySource(value = "classpath:datasource.properties", ignoreResourceNotFound = true) })
 public class CvApiApplication {
 
     public static void main(String[] args) {
