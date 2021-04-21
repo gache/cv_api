356adf92976710a47d8ffd6a908f9feb6d0bafb0
Erick <erickfrancodelgado@hotmail.com>
2021-04-21 17:35:50 +0200

    update class ExperienceServiceImpl

--- src/main/java/fr/erickfranco/cv_api/services/serviceimpl/ExperienceServiceImpl.java
+++ src/main/java/fr/erickfranco/cv_api/services/serviceimpl/ExperienceServiceImpl.java
@@ -1,12 +1,45 @@
 package fr.erickfranco.cv_api.services.serviceimpl;
 
+import fr.erickfranco.cv_api.models.Experience;
+import fr.erickfranco.cv_api.repositories.ExperienceRepository;
+import fr.erickfranco.cv_api.services.serviceinter.IExpericenceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author Erick Franco
  */
 @Service
-public class ExperienceServiceImpl  {
+public class ExperienceServiceImpl implements IExpericenceService {
+
+    @Autowired
+    private final ExperienceRepository experienceRepository;
+
+    public ExperienceServiceImpl(ExperienceRepository experienceRepository) {
+        this.experienceRepository = experienceRepository;
+    }
 
 
+    @Override
+    public List<Experience> findAll() {
+        return experienceRepository.findAll();
+    }
+
+    @Override
+    public Experience findById(Long id) {
+        return experienceRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Experience save(Experience experience) {
+        return experienceRepository.save(experience);
+    }
+
+    @Override
+    public void delete(Long id) {
+        experienceRepository.deleteById(id);
+
+    }
 }
