33bf6fe2d03b70ea9a504e268f57d951c1e60518
Erick <erickfrancodelgado@hotmail.com>
2021-04-21 21:09:14 +0200

    update pom.xml et elimination de la class UserController

--- src/main/java/fr/erickfranco/cv_api/controllers/ProjetController.java
+++ src/main/java/fr/erickfranco/cv_api/controllers/ProjetController.java
@@ -2,9 +2,11 @@ package fr.erickfranco.cv_api.controllers;
 
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api")
 @RestController
 public class ProjetController {
 
